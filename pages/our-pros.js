3f27b5e95633ec4b466940ad5f33963255a40a48
19522059 <19522059@gm.uit.edu.vn>
2022-12-06 15:29:02 +0700

    Add some components

---
+++ pages/our-pros.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import OurAdvantages from '@components/our-pros/our-pros';
+import { MainLayout } from '@layouts/main';
+
+const OurAdvantages=()=> {
+  return (
+    <div>
+        <OurAdvantages />       
+    </div>
+  )
+}
+
+OurAdvantages.layout=MainLayout;
+export default OurAdvantages;
