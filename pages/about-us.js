2a890f0d106df4f90245bd9f87b8d99dd83fa695
19522059 <19522059@gm.uit.edu.vn>
2022-12-07 13:30:40 +0700

    add use-popover.js to hooks folder, add ScrollToTop component to about-us.js

--- pages/about-us.js
+++ pages/about-us.js
@@ -2,12 +2,14 @@ import CommentSection from '@components/about-us/comment-section';
 import HeaderSection from '@components/about-us/header-section';
 import Logo from '@components/about-us/logo';
 import Team from '@components/about-us/team';
+import ScrollToTop from '@components/scroll-to-top/scroll-to-top';
 import { MainLayout } from '@layouts/main'
 import React from 'react'
 
 const AboutUs=()=> {
   return (
     <div>
+      <ScrollToTop />
       <HeaderSection />
       <CommentSection />
       <Logo />
